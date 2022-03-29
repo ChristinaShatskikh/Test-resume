5cfb0c9bc2f9d5af8ec5a912a3247b2465ac0610
ChristinaShatskikh <morenita1907@gmail.com>
2022-03-29 03:50:40 +0300

    Update 2022-03-29T00:50:37.766Z

--- source/js/carousel.js
+++
@@ -1,5 +0,0 @@
-let photoPopup = document.querySelector(".modal-photo");
-let fullPhoto = document.querySelector(".modal-photo__image");
-let closePhoto = document.querySelector(".modal-photo__close");
-let thumbnails = document.querySelectorAll(".portfolio__item");
-
--- source/js/likes.js
+++
@@ -1,5 +0,0 @@
-let heart = document.querySelector(".offer-block__heart-button");
-
-heart.onclick = function() {
-  heart.classList.toggle("added");
-}
